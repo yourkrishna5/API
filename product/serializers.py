ecf7324e8410431b2c669cc3f539626c3715adf5
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-07-12 21:31:08 +0545

    Update serializers.py

--- product/serializers.py
+++ product/serializers.py
@@ -1,40 +1,45 @@
 from rest_framework import serializers
 from drf_extra_fields.fields import Base64ImageField
-from .models import Product, Info
+from .models import Product, Info, Comments
 
-# Product Serializer
+
+# ---------- Product Serializers ----------
 class ProductSerializer(serializers.ModelSerializer):
-    image = Base64ImageField()  # 👈 Base64 support
+    image = Base64ImageField()
 
     class Meta:
         model = Product
         fields = '__all__'
 
-
-# Info Serializer
-class InfoSerializer(serializers.ModelSerializer):
-    profile_picture = Base64ImageField()  # 👈 Base64 support
-
-    class Meta:
-        model = Info
-        fields = '__all__'
-
-
-# Name and Profile Pic only
-class InfoNamePicSerializer(serializers.ModelSerializer):
-    profile_picture = Base64ImageField()  # 👈 Base64 support
-
-    class Meta:
-        model = Info
-        fields = ['name', 'profile_picture']
-
-from rest_framework import serializers
-from drf_extra_fields.fields import Base64ImageField
-from .models import Product
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     image = Base64ImageField()
 
     class Meta:
         model = Product
-        fields = ['title', 'condition', 'brand', 'image',]
+        fields = ['title', 'condition', 'brand', 'image']
+
+
+# ---------- Info Serializers ----------
+class InfoSerializer(serializers.ModelSerializer):
+    profile_picture = Base64ImageField()
+
+    class Meta:
+        model = Info
+        fields = '__all__'
+
+
+class InfoNamePicSerializer(serializers.ModelSerializer):
+    profile_picture = Base64ImageField()
+
+    class Meta:
+        model = Info
+        fields = ['name', 'profile_picture']
+
+
+# ---------- Comment Serializer ----------
+class Cserializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comments
+        fields = '__all__'
+        read_only_fields = ['name']  # assuming you set this automatically in the view
