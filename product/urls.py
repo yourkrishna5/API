6e25a585dfe936e37d6877d7215a61bd34717ddd
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-07-12 13:05:05 +0545

    Update urls.py

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProductCreateView, ApiHomeView
+from .views import ProductCreateView, ApiHomeView, InfoView
 from rest_framework.authtoken import views
 
 urlpatterns = [
     path('create/', ProductCreateView.as_view(), name='product-create'),
     path('', ApiHomeView.as_view(), name='home-doc'),
     path('api-token-auth/', views.obtain_auth_token),  # Token auth endpoint
+    path('info/', InfoView.as_view(), name='info'),    # New endpoint for Info
 ]
