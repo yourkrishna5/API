8c7980e9f17f1779304f6d0bc34f368686f5eef7
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-07-12 12:55:40 +0545

    Update models.py

--- product/models.py
+++ product/models.py
@@ -1,15 +1,44 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Product(models.Model):
     uploader_id = models.CharField(max_length=128)  # Firebase UID
     title = models.CharField(max_length=255)
     brand = models.CharField(max_length=255)
     condition = models.CharField(max_length=100)
-    image_url=models.URLField(default="")
+    image = models.ImageField(upload_to='product_images/')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+class Info(models.Model):
+    status_choices = [
+        ("Koshi Province", "Koshi Province"),
+        ("Madhesh Province", "Madhesh Province"),
+        ("Bagmati Province", "Bagmati Province"),
+        ("Gandaki Province", "Gandaki Province"),
+        ("Lumbini Province", "Lumbini Province"),
+        ("Karnali Province", "Karnali Province"),
+        ("Sudurpashchim Province", "Sudurpashchim Province"),
+    ]
+
+    job_choices = [
+        ("Student", "Student"),
+        ("Seller", "Seller"),
+        ("Engineer", "Engineer"),
+        ("Doctor", "Doctor"),
+        ("Teacher", "Teacher"),
+        ("Artist", "Artist"),
+        ("Developer", "Developer"),
+        ("Farmer", "Farmer"),
+        ("Businessperson", "Businessperson"),
+        ("Others", "Others"),
+    ]
+
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200, choices=status_choices)
+    job = models.CharField(max_length=50, choices=job_choices)
+    profile_picture = models.URLField()
+
+    def __str__(self):
+        return self.name
