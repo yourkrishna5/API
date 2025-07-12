29d23090a09174dbac2706dd4883a43bf5de2c25
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-07-12 12:07:20 +0545

    Update settings.py

--- a/settings.py
+++ a/settings.py
@@ -60,11 +60,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'a.wsgi.application'
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
