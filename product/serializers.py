c07572457293c19d944ae91c144125786b301cbd
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-07-12 13:46:47 +0545

    Update serializers.py

--- product/serializers.py
+++ product/serializers.py
@@ -1,23 +1,29 @@
 from rest_framework import serializers
-from .models import Product
+from drf_extra_fields.fields import Base64ImageField
+from .models import Product, Info
 
+# Product Serializer
 class ProductSerializer(serializers.ModelSerializer):
+    image = Base64ImageField()  # 👈 Base64 support
+
     class Meta:
         model = Product
         fields = '__all__'
 
 
-
-from rest_framework import serializers
-from .models import Info
-
+# Info Serializer
 class InfoSerializer(serializers.ModelSerializer):
+    profile_picture = Base64ImageField()  # 👈 Base64 support
+
     class Meta:
         model = Info
         fields = '__all__'
 
 
+# Name and Profile Pic only
 class InfoNamePicSerializer(serializers.ModelSerializer):
+    profile_picture = Base64ImageField()  # 👈 Base64 support
+
     class Meta:
         model = Info
         fields = ['name', 'profile_picture']
