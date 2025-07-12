dbd0f4932fb50d5b7d02d171f73a6b37005b6cda
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-07-12 22:16:40 +0545

    Update urls.py

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,19 @@
+from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+
+from .views import (
+    ApiHomeView,
+    ProductCreateView,
+    InfoView,
+    ProductDetailView,
+    ProductCommentView,
+)
 
 urlpatterns = [
     path('', ApiHomeView.as_view(), name='api-home'),
     path('create/', ProductCreateView.as_view(), name='product-create'),
     path('info/', InfoView.as_view(), name='info'),
-    path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-    path('<int:pk>/comments/', ProductCommentView.as_view(), name='product-comments'),
-    path('auth-token/', obtain_auth_token, name='api-token-auth'),  # 🔥 This is what you need
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('product/<int:pk>/comments/', ProductCommentView.as_view(), name='product-comments'),
+    path('auth-token/', obtain_auth_token, name='api-token-auth'),  # ✔️ Token Auth URL
 ]
