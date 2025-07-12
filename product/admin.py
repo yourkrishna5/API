93a9de34fb8b5d4dc5e9c934c31f9509c783bb6a
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-07-12 22:10:40 +0545

    Update admin.py

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product,Info
+from .models import Product,Info,Comments
 # Register your models here.
 admin.site.register(Product)
 from django.contrib import admin
@@ -7,3 +7,4 @@ from rest_framework.authtoken.models import Token
 
 admin.site.register(Token)
 admin.site.register(Info)
+admin.site.register(Comments)
