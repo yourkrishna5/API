48cf2d8f4ced427b3173c0980ff3a7c137117dc9
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-07-12 12:03:06 +0545

    Update models.py

--- product/models.py
+++ product/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     brand = models.CharField(max_length=255)
     condition = models.CharField(max_length=100)
-    image_url=models.URLField()
+    image_url=models.URLField(default="")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
