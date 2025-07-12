89b9ec06fef8364ca04f1019f669b880fb230022
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-07-12 22:07:38 +0545

    Update urls.py

--- product/urls.py
+++ product/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('', ApiHomeView.as_view(), name='api-home'),
     path('create/', ProductCreateView.as_view(), name='product-create'),
     path('info/', InfoView.as_view(), name='info'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
     path('<int:pk>/comments/', ProductCommentView.as_view(), name='product-comments'),
 ]
