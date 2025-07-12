2a5c82d6162f88cc557b514315689b0c34e28138
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-07-12 13:01:48 +0545

    Update serializers.py

--- product/serializers.py
+++ product/serializers.py
@@ -5,3 +5,13 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+
+from rest_framework import serializers
+from .models import Info
+
+class InfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Info
+        fields = '__all__'
