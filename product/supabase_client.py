033cc31a160e9b49c46fe512e4e0b7b0347ecba2
yourkrishna5 <cnrkrishnakhanal@gmail.com>
2025-07-12 23:13:04 +0545

    Create supabase_client.py

---
+++ product/supabase_client.py
@@ -0,0 +1,7 @@
+from supabase import create_client, Client
+import os
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
